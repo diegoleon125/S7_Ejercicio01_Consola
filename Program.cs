868ba5f05c11b5d2b6ac6b554db173a39a309f51
user <user@LAPTOP-2DK3J1EI>
2023-10-03 11:22:50 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ingrese el valor de n: ");
+int N = int.Parse(Console.ReadLine());
+float result = 0f;
+float suma = 0f;
+for (int i = 1; i <= N; i++)
+{
+    result = (16f-i) / (3f*i);
+    Console.WriteLine("resultado : " + result);
+    suma += result;
+}
+Console.WriteLine(suma);
